01b505ec33712449f35babce44d94fb2a4190522
Frolov <ilia.frolov@intive.com>
2023-02-18 21:38:07 +0200

    update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import user from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+import App from "./App";
+
+test("can receive a new user and show it on a list", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const testUserName = "TestName";
+  const testUserEmail = "TestEmaill@email.com";
+
+  const nameImput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const button = screen.getByRole("button");
+  user.click(nameImput);
+  user.keyboard(testUserName);
+  user.click(emailInput);
+  user.keyboard(testUserEmail);
+  user.click(button);
+
+  // screen.logTestingPlaygroundURL();
+  await waitFor(() => {
+    screen.debug();
+
+    const name = screen.getByRole("cell", { name: testUserName });
+    const email = screen.getByRole("cell", { name: testUserEmail });
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  });
 });
