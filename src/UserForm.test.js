1afdded49b43b23986161f1d75227a08346339aa
Frolov <ilia.frolov@intive.com>
2023-02-18 21:51:35 +0200

    add clear inputs functionality by TDD

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -1,20 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import UserForm from "./UserForm";
 
-test("it shows two inputs and a button", () => {
-  render(<UserForm />);
-  const imputs = screen.getAllByRole("textbox");
-  const button = screen.getByRole("button");
-  expect(imputs).toHaveLength(2);
-  expect(button).toBeInTheDocument();
-});
-
-test("it calls onUserAdd when form is submitted", () => {
-  const onUserAdd = jest.fn();
-
-  render(<UserForm onUserAdd={onUserAdd} />);
-
+const addUser = () => {
   const testUserName = "TestName";
   const testUserEmail = "TestEmaill@email.com";
 
@@ -32,10 +20,36 @@ test("it calls onUserAdd when form is submitted", () => {
 
   const button = screen.getByRole("button");
   user.click(button);
+  return {
+    user: { name: testUserName, email: testUserEmail },
+    inputs: { nameImput, emailInput },
+  };
+};
+test("it shows two inputs and a button", () => {
+  render(<UserForm />);
+  const imputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+  expect(imputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
 
+test("it calls onUserAdd when form is submitted", () => {
+  const onUserAdd = jest.fn();
+
+  render(<UserForm onUserAdd={onUserAdd} />);
+  const { user } = addUser();
   expect(onUserAdd).toHaveBeenCalled();
-  expect(onUserAdd).toHaveBeenCalledWith({
-    name: testUserName,
-    email: testUserEmail,
+  expect(onUserAdd).toHaveBeenCalledWith(user);
+});
+
+test("empties the two inputs when form is submitted", async () => {
+  render(<UserForm onUserAdd={jest.fn()} />);
+  const {
+    inputs: { nameImput, emailInput },
+  } = addUser();
+
+  await waitFor(() => {
+    expect(nameImput).toHaveValue("");
+    expect(emailInput).toHaveValue("");
   });
 });
